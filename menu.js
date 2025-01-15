cc2b93392a762a6cd1a0fe278366d2e033b3aa6f
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-01-15 17:23:46 +0200

    js

---
+++ menu.js
@@ -0,0 +1,17 @@
+// const body = document.body;
+
+
+// body.onclick( Func() );
+
+// function Func() {
+//     alert('Hello, kitty! Inline event handlers are bad practice. Do not use them!');
+// }
+
+
+
+window.onload = function(){
+    
+    document.getElementsByClassName("Text_Informativ").style.color = "pink";
+}
+
+
